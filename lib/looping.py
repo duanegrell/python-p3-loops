7c37a9a801ad0fa75f99c59cb23d2520d30623bd
Duane Grell <duanegrell@gmail.com>
2023-02-26 15:34:00 -0500

    passing tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    if i == 0:
+        print ("Happy New Year!")
+
+# happy_new_year()
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    list_squared = [int * int for int in int_list]
+    print (list_squared)
+    return (list_squared)
+
+# square_integers(int_list = [1, 2, 3, 4, 5])
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    for i in range(1,101):
+        if (i % 5) == 0 and (i % 3 == 0):
+            print("FizzBuzz")
+        elif (i % 5) == 0:
+            print("Buzz")
+        elif (i % 3) == 0:
+            print("Fizz")
+        else:
+            print(i)
+
+
+fizzbuzz()
